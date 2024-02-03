0941e0b5ab048253bfa928d023f10ec31c5b37fc
GrimdvlDesktop <grimdvl@gmail.com>
2024-02-04 00:47:36 +0300

    35 lesson done

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,12 @@ import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Form from './modules/form';
+import ShowInfo from './modules/showInfo';
+import Download from './modules/download';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({btns: '.next', container: '.page'});
     slider.render();
-
 
     const modulePageSlider = new MainSlider({container: '.moduleapp', btns: '.next'});
     modulePageSlider.render();
@@ -44,4 +45,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
     new Form('.form').init();
+
+    new ShowInfo('.plus__content').init();
+
+    new Download('.download').init();
 });
